c70e24111c2b328ffdaa36fddb74a1b2e17e819c
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2022-10-01 18:50:02 -0300

    fix meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,9 @@ const Home: NextPage = () => {
         <title>Damasio Business</title>
         <meta name="description" content="Grandes investimentos começam aqui" />
         <meta property="og:title" content="Damasio Business" />
-        <meta property="og:description" content="Grandes investimentos começam aqui">
+        <meta property="og:description" content="Grandes investimentos começam aqui"/>
         <meta property="og:url" content="https://app.damasio.mission.dev.br" />
-        <meta property="og:image" content="/logo-social.webp" />
+        <meta property="og:image" content="/logo-social.jpg" />
         <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
